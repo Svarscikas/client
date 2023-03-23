48b00b9dfbe2db0fdd534ea0ab437496107dfb9b
Arnas <arnas.e@gmail.com>
2023-03-23 16:24:01 +0200

    Added leader board functonality

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 function Leaderboard() {
+  const [bestLifts, setBestLifts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/exercises/personalbests').then( (response) =>{
+      setBestLifts(response.data)     //console.log(response.data);
+    });
+   },[])
+
   return (
     <main>
         <div className='Header'>Leaderboard</div>
+          <div className='Card'>
+            <h2 className='WorkoutTitle'>Best Lifts
+            </h2>
+              <div>
+                {bestLifts.length > 0 && 
+                  <table className='exerciseTable'>
+                  <tr>
+                    <th>Exercise</th>
+                    <th>Weight</th>
+                    <th>User</th>
+                  </tr>
+                    {bestLifts.map((value,key) => {
+                      return(
+                        <tr>
+                          <td>{value.exercise}</td>
+                          <td>{value.weight} kg</td>
+                          <td>{value.username}</td>                       
+                        </tr>
+                      )
+                    })}
+                </table>
+                }
+                {bestLifts.length == 0 &&
+                <div style={{textAlign: "center"}}>No data</div>
+                }
+              </div>
+          </div>
     </main>
   )
 }
